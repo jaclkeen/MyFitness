fd86e8ab78fde3c852ec6d8f9ca624f8880b94c7
Jacob Keen <jaclkeen@gmail.com>
2017-01-19 13:13:09 -0600

    finished weekly information

--- src/MyFitness/wwwroot/js/BarChart.js
+++ src/MyFitness/wwwroot/js/BarChart.js
@@ -50,12 +50,15 @@ function CreateBarChartForCaloriesBurnedInTimeRange(CaloricData) {
                     'rgba(255, 206, 86, 0.2)',
                     'rgba(75, 192, 192, 0.2)',
                     'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(353, 102, 255, 0.6)',
+                    'rgba(54, 162, 235, 0.6)'
+
                 ],
                 borderColor: [
                     "black", "black", "black", "black", "black", "black", "black"
                 ],
-                borderWidth: 2,
+                borderWidth: 1,
                 data: CaloricData,
             }
         ]
